f891e0a35488df74b1dfa7a6736e7e364e608969
illyalane <lane_illya@yahoo.com>
2023-07-26 23:02:48 -0400

    completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUp){
+
+if(pickUp > 42){
+
+    return pickUp - 42;
+}
+else {
+
+    return 42 - pickUp
+}
+
+}
+
+function distanceFromHqInFeet(feetValue){
+    return distanceFromHqInBlocks(feetValue) * 264;  
+}
+
+function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+
+    if (start > destination){
+
+        return (start - destination) * 264;
+    }
+    else
+        return (destination - start) * 264;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+    }
+        
